48b3032baee4ee2c62f37bf7f6bd97e75494270c
NazarDzneladze <nazar.dz055@mail.ru>
2022-12-01 13:27:32 +0300

    feat(p_4):add Title component

---
+++ src/components/Title/styles.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const StyledTitle = styled.h3`
+  font-family: "Inter";
+  font-style: normal;
+  font-weight: 700;
+  font-size: 24px;
+  line-height: 29px;
+
+  color: #000000;
+`;
+
+export { StyledTitle };

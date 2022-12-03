b6f9f44c5006ab165196951d8a2f88aabc20d30a
NazarDzneladze <nazar.dz055@mail.ru>
2022-12-03 20:30:14 +0300

    feat(p_4): add some styles and functionality to the TotalBudget component

--- src/components/TotalBudget/TotalBudget.tsx
+++ src/components/TotalBudget/TotalBudget.tsx
@@ -1,8 +1,46 @@
+import { Button } from "components/Button/Button";
+import { Input } from "components/Input/Input";
+import { useBudgetContext } from "context/BudgetContext/BudgetContext";
 import { useCurrenciesContext } from "context/CurrencyContext/CurrencyContext";
-import { StyledTotalBudget } from "./styles";
+import { useInput } from "hooks/useInput";
+import { useState } from "react";
+import { StyledTotalBudget, TotalBudgetInfo } from "./styles";
 
 export const TotalBudget = () => {
+  const currentBudget = useInput();
   const { currentCurrency } = useCurrenciesContext();
+  const { budget, setNewBudget } = useBudgetContext();
+  const [isEditBudget, setIsEditBudget] = useState(false);
 
-  return <StyledTotalBudget>Budget:3000{currentCurrency.value}</StyledTotalBudget>;
+  const handleIsEditBudget = () => {
+    if (isEditBudget) {
+      setIsEditBudget((isEditBudget) => !isEditBudget);
+      setNewBudget(+currentBudget.value);
+    } else {
+      setIsEditBudget((isEditBudget) => !isEditBudget);
+    }
+  };
+
+  return (
+    <StyledTotalBudget>
+      {isEditBudget ? (
+        <>
+          <Input {...currentBudget} placeholder="Enter  budget ..." type="number" $editBudget />
+          <Button handleIsEditBudget={handleIsEditBudget} $budget>
+            Save
+          </Button>
+        </>
+      ) : (
+        <>
+          <TotalBudgetInfo>
+            Budget: {budget}
+            {currentCurrency.value}
+          </TotalBudgetInfo>
+          <Button handleIsEditBudget={handleIsEditBudget} $budget>
+            Edit
+          </Button>
+        </>
+      )}
+    </StyledTotalBudget>
+  );
 };
--- src/components/TotalBudget/styles.ts
+++ src/components/TotalBudget/styles.ts
@@ -2,21 +2,24 @@ import styled from "styled-components";
 
 const StyledTotalBudget = styled.div`
   display: flex;
-  justify-content: center;
+  justify-content: space-between;
   align-items: center;
   grid-column: span 2;
 
   height: 100px;
-
-  font-family: "Inter";
-  font-style: normal;
-  font-weight: 500;
-  font-size: 20px;
-  line-height: 24px;
+  padding: 0 20px 0;
 
   background-color: #7cc6fe;
   border-radius: 10px;
   color: #000000;
 `;
 
-export { StyledTotalBudget };
+const TotalBudgetInfo = styled.p`
+  font-family: "Inter";
+  font-style: normal;
+  font-weight: 500;
+  font-size: 20px;
+  line-height: 24px;
+`;
+
+export { StyledTotalBudget ,TotalBudgetInfo};

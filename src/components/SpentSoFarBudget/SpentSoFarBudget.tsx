e7eb5109cf9bca6143ff420368f9c26d771184e1
NazarDzneladze <nazar.dz055@mail.ru>
2022-12-06 15:27:31 +0300

    feat(p_4): add a calculation of the spent budget

--- src/components/SpentSoFarBudget/SpentSoFarBudget.tsx
+++ src/components/SpentSoFarBudget/SpentSoFarBudget.tsx
@@ -1,12 +1,19 @@
 import { useCurrenciesContext } from "context/CurrencyContext/CurrencyContext";
+import { useExpensesContext } from "context/ExpensesContext/ExpensesContext";
 import { SpentSoFarInfo, StyledSpentSoFarBudget } from "./styles";
 
 export const SpentSoFarBudget = () => {
   const { currentCurrency } = useCurrenciesContext();
+  const { expenses } = useExpensesContext();
+
+  const spentSoFar = expenses.reduce((total, expense) => (total += expense.expenseCost), 0);
 
   return (
     <StyledSpentSoFarBudget>
-      <SpentSoFarInfo>Spent so far: 1000{currentCurrency.value}</SpentSoFarInfo>
+      <SpentSoFarInfo>
+        Spent so far: {spentSoFar}
+        {currentCurrency.value}
+      </SpentSoFarInfo>
     </StyledSpentSoFarBudget>
   );
 };
